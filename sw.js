7efaa99d3e41cc184bed1bad2841748be7283db1
Josephalapod <josephalapodmolluskon@gmail.com>
2026-05-31 18:07:46 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Bump CACHE_VERSION whenever you change index.html so phones pick up the update.
-const CACHE_VERSION = "booklib-v9";
+const CACHE_VERSION = "booklib-v10";
 const SHELL = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (event) => {
